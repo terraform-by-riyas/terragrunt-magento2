7f46bacb3acb8de534656c458dd3c4790f75cf31
Riyas Rawther <rrauther@primahealthcredit.com>
2023-09-17 11:26:11 +0530

    tag

--- infrastructure/modules/aws_network_interface/main.tf
+++ infrastructure/modules/aws_network_interface/main.tf
@@ -1,7 +1,13 @@
 resource "aws_network_interface" "this" {
   subnet_id       = var.aws_subnet_id
   private_ips     = var.private_ips
+
+  tags = merge(var.default_tags,{
+        Name = "Instance-${var.environment_name}${count.index}"
+        },
+    )
+
   tags = {
-    Name = "DB"
+    Name = var.name
   }
   }
