e10b73d1b32abb52aef21ae22723b715bb397c6a
Riyas Rawther <rrauther@primahealthcredit.com>
2023-09-17 14:44:19 +0530

    ec2 #

--- infrastructure/modules/packer/variables.tf
+++ infrastructure/modules/packer/variables.tf
@@ -1,9 +1,9 @@
 variable "ec2" {
   description  = "EC2 instances names and types included in AutoScaling groups"
   default      = {
-    varnish    = "m6g.large"
+ //  varnish    = "m6g.large"
     frontend   = "c6g.xlarge"
-    admin      = "c6g.xlarge"
+ //   admin      = "c6g.xlarge"
    }
 }
 # variable "IAM_INSTANCE_PROFILE" {}
