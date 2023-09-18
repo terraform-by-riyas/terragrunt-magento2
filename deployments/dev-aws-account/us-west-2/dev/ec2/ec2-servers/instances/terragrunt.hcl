110796f41c751f477e5356b2df46e2afddc55f46
Riyas Rawther <rrauther@primahealthcredit.com>
2023-09-18 12:37:38 +0530

    ss

--- deployments/dev-aws-account/us-west-2/dev/ec2/ec2-servers/instances/terragrunt.hcl
+++ deployments/dev-aws-account/us-west-2/dev/ec2/ec2-servers/instances/terragrunt.hcl
@@ -33,7 +33,36 @@ dependency "ec2-instance-connect" {
 
 inputs = {
 
+multiple_instances = {
+    db = {
+      instance_type           = "r7g.large"
+      disable_api_termination = true
+      ebs_optimized           = true
+      ami                     = dependency.aws-data.outputs.ubuntu_arm_graviton_22_04lts
+      key_name                = dependency.ssh-key.outputs.key-name
+      monitoring              = false
+      vpc_security_group_ids  = dependency.sg-db.outputs.security_group_id
+      iam_instance_profile    = dependency.ec2-instance-connect.outputs.iam_profile_name
+      subnet_id               = dependency.vpc.outputs.private_subnets[0]
+      availability_zone       = dependency.vpc.outputs.azs[0]
+ root_block_device = [
+    {
+      encrypted   = true
+      volume_type = "gp3"
+      throughput  = 200
+      volume_size = 50
+      tags = {
+        Name = "my-root-block"
+      }
+    },
+  ]  
+    },
+    varnish = {},
+    redis = {},
+    admin = {},
+    elastic_search = {}
 
+  }
 environment = "dev"
 }
 
--- infrastructure/modules/ec2-instances/main.tf
+++ infrastructure/modules/ec2-instances/main.tf
@@ -1,38 +1,9 @@
-locals {
-  multiple_instances = {
-    one = {
-      instance_type     = "t3.micro"
-      
-      
-      root_block_device = [
-        {
-          encrypted   = true
-          volume_type = "gp3"
-          throughput  = 200
-          volume_size = 50
-          tags = {
-            Name = "my-root-block"
-          }
-        }
-      ]
-    }
-    two = {
-      instance_type     = "t3.small"
-     
-      root_block_device = [
-        {
-          encrypted   = true
-          volume_type = "gp2"
-          volume_size = 50
-        }
-      ]
-    }
-    three = {
-      instance_type     = "t3.medium"
-     
-    }
-  }
+variable "multiple_instances"{
+  type = map(map(object(
+    {root_block_device=map()}
+  )))
 }
+
 
 variable "environment" {}
 
@@ -40,7 +11,7 @@ variable "environment" {}
 module "ec2_multiple" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  for_each = local.multiple_instances
+  for_each = var.multiple_instances
 
     name = "${each.key}"
     root_block_device  = lookup(each.value, "root_block_device", [])
