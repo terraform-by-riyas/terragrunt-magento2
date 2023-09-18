f3a5f831ef28348a8925a4c43e3b52c9f42770ad
Riyas Rawther <rrauther@primahealthcredit.com>
2023-09-18 13:00:23 +0530

    loc

--- infrastructure/modules/ec2-instances/main.tf
+++ infrastructure/modules/ec2-instances/main.tf
@@ -5,7 +5,7 @@ variable "environment" {}
 module "ec2_multiple" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  for_each = var.multiple_instances
+  for_each = local.multiple_instances
 
     name = "${each.key}"
     root_block_device  = lookup(each.value, "root_block_device", [])
