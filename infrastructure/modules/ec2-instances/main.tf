9b061e28df61bc311435987d439fb0c87f6fb95e
Riyas Rawther <rrauther@primahealthcredit.com>
2023-09-18 12:38:25 +0530

    d

--- infrastructure/modules/ec2-instances/main.tf
+++ infrastructure/modules/ec2-instances/main.tf
@@ -14,7 +14,7 @@ module "ec2_multiple" {
   for_each = var.multiple_instances
 
     name = "${each.key}"
-    root_block_device  = lookup(each.value, "root_block_device", [])
+ //   root_block_device  = lookup(each.value, "root_block_device", [])
 tags ={
 Terraform = "True"
 Environment = var.environment
