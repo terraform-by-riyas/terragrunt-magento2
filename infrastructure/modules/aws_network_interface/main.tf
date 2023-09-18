4aff8a6acccbabafe7c43ada5b20a8e137a461a0
Riyas Rawther <rrauther@primahealthcredit.com>
2023-09-18 14:02:52 +0530

    ad description

--- infrastructure/modules/aws_network_interface/main.tf
+++ infrastructure/modules/aws_network_interface/main.tf
@@ -1,6 +1,7 @@
 resource "aws_network_interface" "this" {
   subnet_id       = var.aws_subnet_id
   private_ips     = var.private_ips
+  description = var.description
   
   attachment {
     instance     = var.instance_id
--- infrastructure/modules/aws_network_interface/variables.tf
+++ infrastructure/modules/aws_network_interface/variables.tf
@@ -1,5 +1,8 @@
 variable "aws_subnet_id" {}
 variable "instance_id" {}
+variable "description" {
+  
+}
 variable "name" {}
 variable "private_ips" {
     type = list(string)
